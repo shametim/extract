86aea7c2d577a08df09fb34e01a323708a9158ee
Martin <9932250+shametim@users.noreply.github.com>
2023-01-03 09:45:27 -0500

    what

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ app = Flask(__name__)
 @app.route("/extract", methods=['POST'])
 def extract():
     document = request.data.decode('utf-8')
-    
-    extract = trafilatura.extract(document, output_format='json')
+
+    # extract = trafilatura.extract(document, output_format='json')
 
     app.logger.info("Extraction complete")
 
