5c81646273037b556f5c1efe330fa5968995a5e2
Martin <9932250+shametim@users.noreply.github.com>
2023-01-03 09:20:00 -0500

    maybe

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, request
+import trafilatura
+
+app = Flask(__name__)
+
+
+@app.route("/extract", methods=['POST'])
+def extract():
+    document = request.data.decode('utf-8')
+    
+    extract = trafilatura.extract(document, output_format='json')
+
+    app.logger.info("Extraction complete")
+
+    return extract
